16311968e491689cabde2b332efa51dd6c7bc136
SmartBoy84 <barfie@gabba.ga>
2022-04-17 16:58:33 +0800

    allow for buffer creation without file

--- emojifier.c
+++ emojifier.c
@@ -6,11 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    pixel *Image;
-
-    if (argc < 2 || !(Image = readFile(argv[1])))
-        return 1;
-
     FILE *eF;
     if (argc >= 3)
         generateChart(atoi(argv[2]));
@@ -26,59 +21,65 @@ int main(int argc, char *argv[])
             eF = fopen("emojis.bin", "rb");
     }
 
-    __int32_t imageCount;
-    pixel *emojiBuffer;
-    pixel *averages;
+    pixel *Image;
 
-    readBuffer(eF, &imageCount, &emojiBuffer, &averages);
-
-    printf("Height: %d\nWidth: %d\nEmoji count: %d\n", height(emojiBuffer), width(emojiBuffer), imageCount);
-
-    // CREATE THE DAMNED IMAGE NOW
-    pixel *finalCanvas =
-        createPixelArray(width(Image) * width(emojiBuffer),
-                         height(Image) * height(emojiBuffer)); // Remember height(emojiBuffer) return that of one
-
-    printf("\nImage size: %dx%d\nCanvas size: %dx%d\n", width(Image), height(Image), width(finalCanvas),
-           height(finalCanvas));
-
-    pixel *emoji = createPixelArray(height(emojiBuffer), width(emojiBuffer)); // emoji matching average colour
-
-    // create reference pointers
-    pixel *tempImage = Image;
-    pixel *tempCanvas = finalCanvas; // temp buffer for canvas
-    pixel *tempEmoji;
-
-    pixel *tempInternalCanvas = finalCanvas;
-
-    __int32_t emojiSize = height(emojiBuffer) * width(emojiBuffer); // get pixel # per image
-
-    for (int y = 0; y < height(Image); y++)
+    if (argc >= 2 && (Image = readFile(argv[1])))
     {
-        for (int x = 0; x < width(Image); x++)
+        __int32_t imageCount;
+        pixel *emojiBuffer;
+        pixel *averages;
+
+        readBuffer(eF, &imageCount, &emojiBuffer, &averages);
+
+        printf("Height: %d\nWidth: %d\nEmoji count: %d\n", height(emojiBuffer), width(emojiBuffer), imageCount);
+
+        // CREATE THE DAMNED IMAGE NOW
+        pixel *finalCanvas =
+            createPixelArray(width(Image) * width(emojiBuffer),
+                             height(Image) * height(emojiBuffer)); // Remember height(emojiBuffer) return that of one
+
+        printf("\nImage size: %dx%d\nCanvas size: %dx%d\n", width(Image), height(Image), width(finalCanvas),
+               height(finalCanvas));
+
+        pixel *emoji = createPixelArray(height(emojiBuffer), width(emojiBuffer)); // emoji matching average colour
+
+        // create reference pointers
+        pixel *tempImage = Image;
+        pixel *tempCanvas = finalCanvas; // temp buffer for canvas
+        pixel *tempEmoji;
+
+        pixel *tempInternalCanvas = finalCanvas;
+
+        __int32_t emojiSize = height(emojiBuffer) * width(emojiBuffer); // get pixel # per image
+
+        for (int y = 0; y < height(Image); y++)
         {
-            memcpy(emoji, emojiBuffer + (findEmoji(tempImage, averages, imageCount) * emojiSize),
-                   emojiSize * sizeof(pixel));
-
-            tempEmoji = emoji;
-            tempInternalCanvas = tempCanvas;
-
-            for (int i = 0; i < height(emoji); i++)
+            for (int x = 0; x < width(Image); x++)
             {
-                memcpy(tempInternalCanvas, tempEmoji, width(emoji) * sizeof(pixel));
+                memcpy(emoji, emojiBuffer + (findEmoji(tempImage, averages, imageCount) * emojiSize),
+                       emojiSize * sizeof(pixel));
 
-                tempEmoji += width(emoji);
-                tempInternalCanvas += width(finalCanvas);
+                tempEmoji = emoji;
+                tempInternalCanvas = tempCanvas;
+
+                for (int i = 0; i < height(emoji); i++)
+                {
+                    memcpy(tempInternalCanvas, tempEmoji, width(emoji) * sizeof(pixel));
+
+                    tempEmoji += width(emoji);
+                    tempInternalCanvas += width(finalCanvas);
+                }
+
+                tempImage++;
+                tempCanvas += width(emoji);
             }
 
-            tempImage++;
-            tempCanvas += width(emoji);
+            tempCanvas += (width(finalCanvas) * (height(emoji) - 1));
         }
 
-        tempCanvas += (width(finalCanvas) * (height(emoji) - 1));
+        writeArray(finalCanvas, "output.bmp");
+        return 0;
     }
 
-    writeArray(finalCanvas, "output.bmp");
-
-    return 0;
+    return 1;
 }
